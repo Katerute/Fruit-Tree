04615679c04e24b998b40bc52744bf14e0f804a5
Katerute <39352547+Katerute@users.noreply.github.com>
2018-05-17 00:49:03 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Korzinka = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblPoints = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblLife = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Korzinka)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Korzinka
+            // 
+            this.Korzinka.BackColor = System.Drawing.Color.Transparent;
+            this.Korzinka.Image = global::Projekt.Properties.Resources.Korzinka3;
+            this.Korzinka.Location = new System.Drawing.Point(305, 611);
+            this.Korzinka.Name = "Korzinka";
+            this.Korzinka.Size = new System.Drawing.Size(165, 81);
+            this.Korzinka.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Korzinka.TabIndex = 0;
+            this.Korzinka.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lblPoints,
+            this.toolStripStatusLabel2,
+            this.lblLife});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 711);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(739, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(40, 17);
+            this.toolStripStatusLabel1.Text = "Points";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(13, 17);
+            this.lblPoints.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(26, 17);
+            this.toolStripStatusLabel2.Text = "Life";
+            // 
+            // lblLife
+            // 
+            this.lblLife.Name = "lblLife";
+            this.lblLife.Size = new System.Drawing.Size(19, 17);
+            this.lblLife.Text = "10";
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.BackColor = System.Drawing.Color.DarkGreen;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 705);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(771, 10);
+            this.pnlBottom.TabIndex = 7;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Enabled = false;
+            this.pictureBox3.Image = global::Projekt.Properties.Resources.plus101;
+            this.pictureBox3.Location = new System.Drawing.Point(1136, 573);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(92, 81);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projekt.Properties.Resources.vectorflowertree;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(739, 733);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Korzinka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Korzinka)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Korzinka;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblPoints;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel lblLife;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        //objects
+        class MyClass
+        {
+            public PictureBox obj { get; set; }
+            public Timer objTimer { get; set; }
+        }
+        //Korzinka position
+        int KposX;
+        int KposY;
+        //movement
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        //
+        int Points = 0;
+        int life = 10;
+        int T = 10;
+
+        Random rnd = new Random();
+
+        List<MyClass> objektai;
+        private void CreateObjects()
+        {
+            objektai = new List<MyClass>()
+        {
+            new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.A,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 450, Tag="Apple"
+                }
+            },
+            new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.A,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 410, Tag="Apple2"
+                }
+            },
+            new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.A,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 239, Tag="Apple3"
+                }
+            },
+            new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.BadA,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 577, Tag="BadApple"
+                }
+            },
+              new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.pear,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width- 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 480, Tag="Pear"
+                }
+            },
+              new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.pear,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width- 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 650, Tag="Pear2"
+                }
+            },
+              new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.pear,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width- 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 390, Tag="Pear3"
+                }
+            },
+             new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.BadPear,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 333, Tag="BadPear"
+                }
+            },
+             
+             new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.minus10,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 1000, Tag="minus10"
+                }
+            },
+             new MyClass()
+            {
+                obj=new PictureBox()
+                {
+                    Image = Properties.Resources.plus10,
+                    Size=new Size(60,60),
+                    SizeMode= PictureBoxSizeMode.StretchImage,
+                    BackColor=Color.Transparent,
+                    Location=new Point(rnd.Next(100, this.Width - 160),rnd.Next(100, this.Height / 2))
+                },
+                objTimer = new Timer()
+                {
+                    Interval = 780, Tag="plus10"
+                }
+            },
+
+
+        };
+            for (int i = 0; i < objektai.Count; i++)
+            {
+                objektai[i].objTimer.Tick += obj_Time;
+                this.Controls.Add(objektai[i].obj);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateNewPosition();
+            //функции, чтобы объекты нормально падали
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            //добавляем объекты на экран и заставляем двигаться(падать)
+            CreateObjects();
+
+        }
+        private void obj_Time(object sender, EventArgs e)
+        {
+            var i = objektai.FindIndex(a => a.objTimer.Tag == ((Timer)sender).Tag);
+            objektai[i].obj.Location = new Point(objektai[i].obj.Location.X, objektai[i].obj.Location.Y + T);
+            Position(objektai[i]);
+
+        }
+
+        void GenerateNewPosition()
+        {
+            KposY = this.Bottom - Korzinka.Height - 60;
+            KposX = this.Width / 2;
+            Korzinka.Location = new Point(KposX, KposY);
+            Korzinka.BringToFront();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (GetAsyncKeyState(Keys.Right) < 0 || GetAsyncKeyState(Keys.Left) < 0)
+            {
+
+                StartAppleFalling();
+                if (GetAsyncKeyState(Keys.Right) < 0)
+                {
+                    KposX += 10;
+                    if (Korzinka.Location.X + Korzinka.Width + 20 >= this.Width)
+                    {
+                        KposX -= 10;
+                    }
+                }
+                if (GetAsyncKeyState(Keys.Left) < 0)
+                {
+                    KposX -= 10;
+                    if (Korzinka.Location.X <= 0)
+                    {
+
+                        KposX += 10;
+                    }
+                }
+                Korzinka.Location = new Point(KposX, KposY);
+            }
+        }
+
+        private void CheckWin()
+        {
+
+            if (Points < 0 || life == 0)
+            {
+                for (int i = 0; i < objektai.Count; i++)
+                {
+                    objektai[i].objTimer.Stop();
+                }
+                DialogResult result = MessageBox.Show("You Lose :( Do you want to try again?", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    for (int i = 0; i < objektai.Count; i++)
+                    {
+                        objektai[i].obj.Visible = false;
+                    }
+                    Points = 0;
+                    T = 10;
+                    life = 10;
+                    Updatelabels();
+                    GenerateNewPosition();
+                    CreateObjects();
+                }
+                else this.Close();
+            }
+        }
+
+        private void Updatelabels()
+        {
+            lblPoints.Text = Points.ToString();
+            lblLife.Text = life.ToString();
+        }
+
+        private void StartAppleFalling()
+        {
+            for (int i = 0; i < objektai.Count; i++)
+            {
+                objektai[i].objTimer.Enabled = true;
+            }
+        }
+
+        private void Position(MyClass sender)
+        {
+
+            if (Korzinka.Bounds.IntersectsWith(sender.obj.Bounds))
+            {
+                if (sender.objTimer.Tag.ToString() == "Apple" || sender.objTimer.Tag.ToString() == "Apple2" || sender.objTimer.Tag.ToString() == "Apple3" || sender.objTimer.Tag.ToString() == "Pear" || sender.objTimer.Tag.ToString() == "Pear2" || sender.objTimer.Tag.ToString() == "Pear3")
+                {
+                    Points++;
+                }
+                if (sender.objTimer.Tag.ToString() == "BadApple" || sender.objTimer.Tag.ToString() == "BadPear" || sender.objTimer.Tag.ToString() == "BadApple2" || sender.objTimer.Tag.ToString() == "BadPear2")
+                {
+                    Points--;
+                }
+                if (sender.objTimer.Tag.ToString() == "minus10")
+                {
+                    Points -= 10;
+                }
+                if (sender.objTimer.Tag.ToString() == "plus10")
+                {
+                    Points += 10;
+                }
+                Updatelabels();
+                sender.obj.Location = new Point(rnd.Next(100, this.Width - 100), rnd.Next(100, this.Height / 2 - 100));
+                CheckSpeed();
+            }
+            if (sender.obj.Bounds.IntersectsWith(pnlBottom.Bounds))
+            {
+                if (sender.objTimer.Tag.ToString() == "Apple" || sender.objTimer.Tag.ToString() == "Apple2" || sender.objTimer.Tag.ToString() == "Apple3" || sender.objTimer.Tag.ToString() == "Pear" || sender.objTimer.Tag.ToString() == "Pear2" || sender.objTimer.Tag.ToString() == "Pear3")
+                {
+                    life--;
+                    Updatelabels();
+                }
+                sender.obj.Location = new Point(rnd.Next(100, this.Width - 100), rnd.Next(100, this.Height / 2));
+                CheckSpeed();
+            }
+            CheckWin();
+        }
+
+        private void CheckSpeed()
+        {
+            if (Points > 1 && Points % 10 == 0)
+            {
+                T += 1;
+            }
+        }
+    }
+}
